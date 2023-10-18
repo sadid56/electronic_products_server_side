ed1c4b286f835044a0d0287c42e6c98a9978ff26
sadid56 <sadidhasan56@gmail.com>
2023-10-18 14:59:29 +0600

    added post method

--- index.js
+++ index.js
@@ -1,11 +1,62 @@
 const express = require('express');
 const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
 
 //! middleware
 app.use(cors())
 app.use(express.json())
+
+
+//! CRUD operation
+//WcptlQVUQMp1fxKA
+//assignment-10
+
+
+const uri = "mongodb+srv://assignment-10:WcptlQVUQMp1fxKA@cluster0.dzbhwpo.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    
+    const productCollection = client.db('assignment-10').collection('products');
+    //! POST
+    app.post('/products', async(req, res)=>{
+        const product = req.body;
+        const result = await productCollection.insertOne(product)
+        res.send(result)
+    })
+
+    //! GET    
+    app.get('/products', async(req, res)=>{
+        const result = await productCollection.find().toArray()
+        res.send(result)
+    })
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res)=>{
     res.send('Assignment 10 server is running')
